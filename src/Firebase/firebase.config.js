9994ffbf060405501a87e0135816b2d2ff60599e
Ashik Das <ashikdas562.com>
2022-05-24 14:19:12 +0600

    first commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDn1ozEGqfRNxugrE-NT14i1Q7N2yO7Z8I",
+    authDomain: "ema-john-simple-6f2d1.firebaseapp.com",
+    projectId: "ema-john-simple-6f2d1",
+    storageBucket: "ema-john-simple-6f2d1.appspot.com",
+    messagingSenderId: "728960078166",
+    appId: "1:728960078166:web:deed29022a8a684bd446f7"
+  };
+
+
+  export default firebaseConfig;
