15227e3d67e37050ca21ad3fd193ef6dd0b87d3c
Ashik Das <ashikdas562.com>
2022-05-28 01:09:29 +0600

    commit again

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Login.css'
 
 const Login = () => {
     return (
-        <div>
+        <div className='login-form'>
             <div>
+                <h2>Login</h2>
                   <form action="" onSubmit="">
                      <input type="email" name="" id="" placeholder='Your email'/>
                      <br />
@@ -11,7 +14,12 @@ const Login = () => {
                      <br />
                      <input type="submit" value="submit" />
                   </form>
-                  <p>New to Ema John <link to="/register">Create Account</link></p>
+                  <p>New to Ema John? <Link to="/register">Create Account</Link></p>
+                  <div>      
+                     -------------or---------------
+                  </div>
+
+                  <button className='btn-regular'>Google Sing In</button>
             </div>
 
         </div>
---
+++ src/components/Register/Register.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Register.css'
+
+const Register = () => {
+    return (
+        <div className='register-form'>
+            <div>
+                <h2>Create Account</h2>
+                <form>
+                    <input type="email" name="" id="" placeholder='your email' />
+                    <br />
+                    <input type="password" name="" id="" placeholder='Enter password' />
+                    <br />
+                    <input type="password" name="" id="" placeholder=' re-enter password' />
+                    <br />
+                    <input type="submit" value="submit" />
+                    </form>
+                    <p>Already have an Account? <Link to='/login'>login</Link> </p>
+                   
+            </div>
+        </div>
+    );
+};
+
+export default Register;
