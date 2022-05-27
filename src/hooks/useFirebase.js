274dd42dec1ea9ac81446f81d881ab355f130eac
Ashik Das <ashikdas562.com>
2022-05-28 01:47:14 +0600

    omce more

---
+++ src/hooks/useFirebase.js
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react"
+import { getAuth, signInWithPopup, GoogleAuthProvider ,onAuthStateChanged , signOut } from "firebase/auth";
+
+
+const useFirebase = () =>{
+    const [user, setUser] = useState({});
+    
+    const auth = getAuth();
+    const googleProvider = new GoogleAuthProvider();
+
+    const singInUsingGoogle = ()=>{
+        singInUsingGoogle(auth , googleProvider)
+        .then(result =>{
+            console.log(result.user);
+        })
+    }
+
+    const logOut = () =>{
+        signOut(auth)
+        .then(()=>{
+
+        })
+    }
+
+    useEffect( ()=>{
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+           setUser(user);
+              // ...
+            } 
+            else {
+              // User is signed out
+              // ...
+            }
+          });
+    },[])
+
+    return {
+        user,
+     singInUsingGoogle,
+     signOut
+    }
+}
+
+export default useFirebase;
+
